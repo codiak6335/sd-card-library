c649d7c720699d3a8b8fe80fbf99b562548d72b0
Charles Jones <beaverlakene@gmail.com>
2015-08-08 11:03:16 -0700

    Update sd2-card.h

--- firmware/sd2-card.h
+++ firmware/sd2-card.h
@@ -26,6 +26,21 @@
 #include "sd2-card-config.h"
 #include "sd-info.h"
 
+#if defined(PLATFORM_ID)  //Only defined if a Particle device
+  #include "application.h"
+  STM32_Pin_Info* PIN_MAP = HAL_Pin_Map(); // Pointer required for highest access speed
+#if (PLATFORM_ID == 0)  // Core
+  #define pinLO(_pin) (PIN_MAP[_pin].gpio_peripheral->BRR = PIN_MAP[_pin].gpio_pin)
+  #define pinHI(_pin) (PIN_MAP[_pin].gpio_peripheral->BSRR = PIN_MAP[_pin].gpio_pin)
+#elif (PLATFORM_ID == 6) // Photon
+  #define pinLO(_pin) (PIN_MAP[_pin].gpio_peripheral->BSRRH = PIN_MAP[_pin].gpio_pin)
+  #define pinHI(_pin) (PIN_MAP[_pin].gpio_peripheral->BSRRL = PIN_MAP[_pin].gpio_pin)
+#else
+  #error "*** PLATFORM_ID not supported by this library. PLATFORM should be Core or Photon ***"
+#endif
+#endif
+
+
 #define SPI_BUFF_SIZE 512
 
 /** Set SCK to max rate of F_CPU/2. See Sd2Card::setSckRate(). */
